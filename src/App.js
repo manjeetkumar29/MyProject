702cf1ef9de438266da3c7fa2c63e0f640f432d4
Manjeet_Kumar <manjeetkumar36942336@gmail.com>
2023-11-07 19:07:43 +0530

    added footer and herosection

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
 import React from 'react'
 import Navbar from './Components/Navbar'
+import Footer from './Components/Footer';
+import Herosection from './Components/Herosection';
 
 function App() {
   return (
     <>
+    
     <Navbar></Navbar>
+    <div className=' relative'>
+    <div className='bg absolute'></div>
+    <Herosection></Herosection>
+    </div>
+    <div className='p-[200px]'>
+    <Footer></Footer>
+
+    </div>
+
+    
     </>
   );
 }
